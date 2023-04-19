2ff693f700a673621026e0539f9f874192bcf09d
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2023-04-19 11:49:00 -0600

    Moment before disaster

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[Serializable]
+public class Enemy
+{
+    public GameObject prefabs;
+    [Range(0f, 100f)] public float possiblilty = 100;
+    [HideInInspector] public double _weight;
 
+}
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] Enemy[] enemies;
+    private double accumulation;
+    private System.Random ran = new System.Random();
+
+    private void Awake()
+    {
+        Calculations(); 
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 20; i++)
+        {
+            SpawnRandomEnemy(new Vector3(UnityEngine.Random.Range(-50, 50), 0, UnityEngine.Random.Range(-50, 50)));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnRandomEnemy(Vector3 position)
     {
-        
+        Enemy randomEnemy = enemies[GetRandomEnemyIndexOf()];
+        Instantiate(randomEnemy.prefabs, position,Quaternion.identity, transform);
+
+    }
+
+    private int GetRandomEnemyIndexOf()
+    {
+        double r = ran.NextDouble() * accumulation;
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i]._weight >= r)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    private void Calculations()
+    {
+        accumulation = 0f;
+        foreach (Enemy enemy in enemies) 
+        {
+            accumulation += enemy.possiblilty;
+            enemy._weight = accumulation;
+        }
     }
 }
