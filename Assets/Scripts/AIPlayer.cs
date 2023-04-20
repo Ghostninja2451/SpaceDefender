5095fe9c93e3aff50c2a5bf92d5978f8451d1dd7
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2023-04-20 11:49:23 -0600

    AI is now functional

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AIPlayer : MonoBehaviour
+{
+    [SerializeField] float maxSpeed;
+    private float speed;
+    
+    private Collider[] hitColliders;
+    private RaycastHit hitRays;
+    
+    [SerializeField] float sightRange;
+    [SerializeField] float detectionRange;
+
+    [SerializeField] Rigidbody rg;
+    [SerializeField] GameObject target;
+
+    private bool playerSpotted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = maxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!playerSpotted)
+        {
+            hitColliders = Physics.OverlapSphere(transform.position, detectionRange);
+            foreach (var collider in hitColliders)
+            {
+                if (collider.tag == "Player")
+                {
+                    target = collider.gameObject;
+                    playerSpotted = true;
+                }
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(transform.position, (target.transform.position - transform.position), out hitRays, sightRange))
+            {
+                if (hitRays.collider.tag == "Player")
+                {
+                    playerSpotted = false;
+                }
+                else
+                {
+                    var head = target.transform.position - transform.position;
+                    var distance = head.magnitude;
+                    var direction = head / distance;
+
+
+                    Vector3 move = new Vector3(direction.x * speed, 0, direction.z * speed);
+                    rg.velocity = move;
+                    transform.forward = move;
+                }
+            }
+        }
+    }
+}
