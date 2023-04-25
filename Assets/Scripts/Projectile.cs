1404202ef3f10133ac44a68aeb959438fc0d9b55
Ghostninja2451 <80051664+Ghostninja2451@users.noreply.github.com>
2023-04-25 17:19:49 -0600

    Finally got the speedy enemy fix

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -8,10 +9,12 @@ public class Projectile : MonoBehaviour
 
     public void Update()
     {
-        Destroy(gameObject, 3);
+        Destroy(gameObject, 1);
+        
     }
     private void OnCollisionEnter(Collision collision)
     {
+        //Debug.Log("Hit");
         Destroy(gameObject);
     }
 }
